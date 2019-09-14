7991bd0acbd64587c91a2a919623991a0134dec5
KG ZZ <czheng5h88@gmail.com>
2019-09-14 18:06:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].each do |holiday, supplies|
 end
 
 def all_supplies_in_holidays(holiday_hash)
