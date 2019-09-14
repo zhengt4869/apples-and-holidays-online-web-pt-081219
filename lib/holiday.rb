eb2df276dcaa65735598e3bcfa46f243254bdbff
KG ZZ <czheng5h88@gmail.com>
2019-09-14 18:49:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,10 +88,14 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  res = []
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      
+      if supplies.include?("BBQ")
+        res << holiday
+    end 
   end
+  res
 end
 
 
