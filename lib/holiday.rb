77f954a0dc89847fdc354c3f4744afef65209a66
KG ZZ <czheng5h88@gmail.com>
2019-09-14 18:23:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+  holiday_hash.each do |season, holidays|
+    puts season + ":"
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
