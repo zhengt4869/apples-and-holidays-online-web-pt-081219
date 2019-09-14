8dc063895d56b907866efb7c7fd805ec14d96edd
KG ZZ <czheng5h88@gmail.com>
2019-09-14 18:34:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts season.to_s.capitalize + ":"
     holidays.each do |holiday, supplies|
-      res = "\t"+ holiday.to_s.capitalize + ": "
+      res = "  "+ holiday.to_s.capitalize + ": "
       supplies.each do |supply|
         res += supply.capitalize
       end 
